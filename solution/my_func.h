b06c5f83e21e767d691de03b110b85f93eabbd06
IceHole <89043120+IceHoles@users.noreply.github.com>
2021-11-15 08:43:47 +0300

    Delete solution directory

--- solution/my_func.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-double myExp(double x, int N, double* eps);
-double myLn(double x, int N, double* eps);
-double mySh(double x, int N, double* eps);
-double myCh(double x, int N, double* eps);
-double myTan(double x, int N, double* eps);
