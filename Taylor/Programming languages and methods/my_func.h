5cb1a08462d659b56c1fa89aa73fcd16bf706c1b
Чезганов Иван <icezgan21@gmail.com>
2021-11-15 08:49:48 +0300

    Add project files.

---
+++ Taylor/Programming languages and methods/my_func.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+double myExp(double x, int N, double* eps);
+double myLn(double x, int N, double* eps);
+double mySh(double x, int N, double* eps);
+double myCh(double x, int N, double* eps);
+double myTan(double x, int N, double* eps);
